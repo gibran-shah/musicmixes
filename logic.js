419ec9193ff38eb8b16f631746105a1eeae1e4c2
gibran-shah <gshah@assertivesolutions.ca>
2024-01-22 18:13:04 -0700

    Injecting list of mixes in foreground container

--- logic.js
+++ logic.js
@@ -4,12 +4,30 @@ function startApp() {
         'GET',
         null,
         (response) => {
-            console.log('success');
+            const list = JSON.parse(response);
+            const groomedList = list.map(item => item.split('.'))
+                .sort((item1, item2) => {
+                    const num1 = parseInt(item1[0], 10);
+                    const num2 = parseInt(item2[0], 10);
+                    return num1 > num2 ? 1 : -1;
+                })
+                .map(item => item[1]);
+            injectMixList(groomedList);
         },
         (error) => {
             console.log(error);
         }
     );
+}
+
+function injectMixList(list) {
+    const foreGroundContainer = document.querySelector('.foreground-container');
+    list.forEach(l => {
+        const link = document.createElement('a');
+        link.setAttribute('href', '#');
+        link.innerHTML = l;
+        foreGroundContainer.appendChild(link);
+    });
 }
 
 startApp();
